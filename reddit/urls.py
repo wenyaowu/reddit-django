3bee06c6b0bb92e7a16805e2d1e88d415f1cb5c2
wenyaowu <evanwu0225@gmail.com>
2015-04-23 11:20:35 -0700

    Initial Commitment

---
+++ reddit/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'reddit-django.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', views.index, name='index'),
+)
