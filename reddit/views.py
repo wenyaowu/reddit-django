7ce15b6e499e9f577edb1f12ffe83b6af98ad607
wenyaowu <evanwu0225@gmail.com>
2015-04-23 11:40:28 -0700

    Create Post Model

--- reddit/views.py
+++ reddit/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'reddit/index.html')
+    context_dict = {}
+    posts = Post.objects.all()
+    context_dict['posts'] = posts
+
+    return render(request, 'reddit/index.html', context_dict)
