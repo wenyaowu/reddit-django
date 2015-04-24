2e990c715a5678549a2ad94f0a127d5792a57c41
wenyaowu <evanwu0225@gmail.com>
2015-04-23 18:07:57 -0700

    Add base template and integrate into other templates. Use Bootstrap.

--- reddit-django/settings.py
+++ reddit-django/settings.py
@@ -83,6 +83,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
 
 TEMPLATE_PATH = os.path.join(BASE_DIR,  'templates')
 TEMPLATE_DIRS = (
