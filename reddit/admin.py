0ead6ba40f0c6fb45a248af656ae6869bd819ed2
wenyaowu <evanwu0225@gmail.com>
2015-04-23 20:44:50 -0700

    Add Subreddit Model

--- reddit/admin.py
+++ reddit/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from models import Post
+from models import Post, Subreddit
 # Register your models here.
 
+class SubredditAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+
 admin.site.register(Post)
+# Update the registeration to include this customised interface
+admin.site.register(Subreddit, SubredditAdmin)
--- reddit/models.py
+++ reddit/models.py
@@ -1,18 +1,31 @@
 from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Subreddit(models.Model):
+
+    name = models.CharField(max_length=256, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Subreddit, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Post(models.Model):
 
     title = models.CharField(max_length=1024, unique=False)
     pub_datetime = models.DateTimeField(default=datetime.now, blank=True)
-    # user = models.ForeignKey(user)
+    user = models.ForeignKey(User)
     url = models.URLField()
     votes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
-    # tag = models.ManyToManyField(tag)
+    subreddit = models.ForeignKey(Subreddit)
 
     def __unicode__(self):
         return self.title
